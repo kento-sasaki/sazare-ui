749cc82d53557682d589802af26a7d00b780160d
kento sasaki <kozukata1993@gmail.com>
2026-07-11 01:41:41 +0900

    chore: ESLint/Prettier/cSpell/Huskyの設定を追加
    要件9〜11番（ESLint/Prettier/cSpell採用、commit前の自動実行）に対応する。
    origin/setupブランチの設定を参考にしつつ、flat configとworkspace構成に合わせて調整した。

---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import js from '@eslint/js'
+import storybook from 'eslint-plugin-storybook'
+import importPlugin from 'eslint-plugin-import'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['**/dist/**', '**/styled-system/**', '**/.storybook-static/**'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      globals: globals.browser,
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      import: importPlugin,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react/jsx-curly-brace-presence': ['error', { props: 'never', children: 'never' }],
+      'react/self-closing-comp': ['error', { component: true, html: true }],
+      'react/jsx-boolean-value': ['error', 'never'],
+      'react-hooks/exhaustive-deps': 'error',
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+      'import/no-duplicates': 'error',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': {
+        typescript: true,
+        node: true,
+      },
+    },
+  },
+  storybook.configs['flat/recommended'],
+)
